920301949eb82ba55e9cd9bb6f62014927e2e420
Gregory Anderson <anderson.gregory.mail@gmail.com>
2019-12-03 21:51:16 -0700

    Add styling for main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import './Main.css'
-
+import React from "react";
+import "./Main.css";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
 
 const Main = () => {
-    return (
-        <section className="Main">
-            <img src={require('./man.png')} id='main__img'/>
-            <h1>Gregory Anderson</h1>
-            <h1>Software Engineer</h1>
+  return (
+    <section className="Main">
+      <img src={require("./man.png")} id="main__img" />
+      <h1>Gregory Anderson</h1>
+      <h1>Software Engineer</h1>
+      <section className="main--links">
+        <section className="main--link">
+          <FaGithub size={24}/>
         </section>
-    )
-}
+        <section className="main--link">
+          <FaLinkedinIn size={24}/>
+        </section>
+      </section>
+    </section>
+  );
+};
 
 export default Main;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,45 @@
-import React from 'react'
-import './Nav.css'
+import React from "react";
+import {
+  AiOutlineLineChart,
+  AiOutlineHome,
+  AiOutlineLaptop,
+  AiOutlineMessage,
+  AiOutlineSound,
+  AiOutlineForm, 
+  AiOutlineRocket
+} from "react-icons/ai";
+import "./Nav.css";
 
 const Nav = () => {
-    return (
-        <section className='Nav'>
-            <h1>Gregory.</h1>
-            <p>Home</p>
-            <p>Experience</p>
-            <p>Projects</p>
-            <p>Journal</p>
-            <p>Contact</p>
+  return (
+    <section className="Nav">
+      <section className="nav__section--links" id="nav--header">
+        <AiOutlineRocket size={64} color="#EB6D4A" /> 
+      </section>
+      <section id="nav--links">
+        <section className="nav__section--links">
+          <AiOutlineHome size={32} color="#EB6D4A" />
+          <p className="nav__p--links">Home</p>
         </section>
-    )
-}
+        <section className="nav__section--links">
+          <AiOutlineLineChart size={32} color="#EB6D4A" />
+          <p className="nav__p--links">Experience</p>
+        </section>{" "}
+        <section className="nav__section--links">
+          <AiOutlineLaptop size={32} color="#EB6D4A" />
+          <p className="nav__p--links">Projects</p>
+        </section>{" "}
+        <section className="nav__section--links">
+          <AiOutlineForm size={32} color="#EB6D4A" />
+          <p className="nav__p--links">Journal</p>
+        </section>{" "}
+        <section className="nav__section--links">
+          <AiOutlineSound size={32} color="#EB6D4A" />
+          <p className="nav__p--links">Contact</p>
+        </section>
+      </section>
+    </section>
+  );
+};
 
 export default Nav;
